fd2d2dca41603dbb82af58e96bb66cbaed26dcc5
Lasse Haffke <lasse@haffke.me>
2022-06-30 23:51:08 +0200

    Toggle icon on dark-mode-toggle button

--- src/js/darkmode.js
+++ src/js/darkmode.js
@@ -4,17 +4,31 @@ const darkTheme = "dark-theme";
 
 let darkMode = localStorage.getItem(localStorageName);
 const darkModeToggle = document.querySelector("#dark-mode-toggle");
+const sunIcon = document.querySelector("#sun-icon");
+const moonIcon = document.querySelector("#moon-icon");
 
 const enableDarkMode = () => {
   document.body.classList.remove(lightTheme);
   document.body.classList.add(darkTheme);
   localStorage.setItem(localStorageName, true);
+  setDisplayedIconSun();
 };
 
 const disableDarkMode = () => {
   document.body.classList.remove(darkTheme);
   document.body.classList.add(lightTheme);
   localStorage.removeItem(localStorageName);
+  setDisplayedIconMoon();
+};
+
+const setDisplayedIconSun = () => {
+  moonIcon.style.gridRow = "";
+  sunIcon.style.gridRow = "1";
+};
+
+const setDisplayedIconMoon = () => {
+  sunIcon.style.gridRow = "";
+  moonIcon.style.gridRow = "1";
 };
 
 if (darkMode) {
